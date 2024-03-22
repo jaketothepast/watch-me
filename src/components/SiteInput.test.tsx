5ed7f1f5cc6f52640b1da4a5aa0f46225a3b5070
jaketothepast <windlejacob12@gmail.com>
2024-03-22 15:17:20 -0400

    SiteInput now saves input text

--- src/components/SiteInput.test.tsx
+++ src/components/SiteInput.test.tsx
@@ -2,10 +2,6 @@ import { expect, test } from "vitest"
 import { render } from "@testing-library/react"
 import SiteInput from "./SiteInput";
 
-test('adds numbers', () => {
-    expect(1 + 2).toBe(3);
-});
-
 test('properly renders', async () => {
     render(<SiteInput />);
 });
--- src/components/SiteInput.tsx
+++ src/components/SiteInput.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react"
+
 /**
  * Handle the input/saving of one single site.
  */
 export default function SiteInput() {
-    return <p>Loading</p>;
+    // Our site we are trying to store.
+    const [site, setSite] = useState("");
+
+    function handleInput(e) {
+        setSite(e.target.value);
+    }
+
+    return (
+        <input type="text" placeholder="Enter blockable website" onInput={handleInput} />
+    );
 }
