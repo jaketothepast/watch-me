246424f6a08891ec112b14195546da4b194d568f
jaketothepast <windlejacob12@gmail.com>
2024-03-22 22:08:19 -0400

    Adding test dependency for firing events in DOM

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
 import { expect, test } from "vitest"
-import { render, screen } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
 import App from "./App"
 
 test("renders App", async () => {
-   render(<App />);
-})
+    render(<App />);
+});
 
 test("has site input", async () => {
     // Arrange
@@ -14,4 +14,22 @@ test("has site input", async () => {
     // Assert
     const item = screen.getAllByLabelText("Enter site to block:");
     expect(item).toHaveLength(1);
-})
+});
+
+test("it saves sites", async () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+    const input = screen.getByLabelText("Enter site to block:");
+    fireEvent.input(input, { target: { value: "Another site" } });
+    fireEvent.click(screen.getByText("Submit"),
+        new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true
+        }));
+
+    // Assert
+    const items = screen.getAllByText("Another site");
+    expect(items).toHaveLength(1);
+});
