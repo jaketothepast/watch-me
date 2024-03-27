30b45e81c5736cdbab449e3dc5c304bd7531558c
jaketothepast <windlejacob12@gmail.com>
2024-03-27 10:24:40 -0400

    Setup web request filters for service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,8 +1,31 @@
 /// <reference types="chrome"/>
+//
+//
+type WebRequestListener = (d: chrome.webRequest.WebRequestBodyDetails) => void;
+function blockListener(details: chrome.webRequest.WebRequestBodyDetails): void {
+    console.log(details.url.toString());
+}
+
+// Initial empty request filter.
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-      console.log(details.url.toString())
-    },
-    {urls: ['https://*/']},
+    blockListener,
+    {urls: []},
     []
 );
+
+/**
+ * Create a new web request filter when new sessions are started.
+ **/
+function makeWebRequestListener(sites: string[]): [WebRequestListener, chrome.webRequest.RequestFilter] {
+    return [blockListener, {urls: sites}]
+}
+
+chrome.storage.onChanged.addListener((changes) => {
+  for (let [key, { newValue }] of Object.entries(changes)) {
+      if (key === 'session') {
+          chrome.webRequest.onBeforeRequest.removeListener(blockListener);
+          const [listener, filter] = makeWebRequestListener(newValue.sites);
+          chrome.webRequest.onBeforeRequest.addListener(listener, filter);
+      }
+  }
+});
